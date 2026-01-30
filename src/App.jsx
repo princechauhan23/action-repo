bca8dd609d8d050af231359d34966cbebcf0ee8f
Prince Chauhan <princechauhan546@gmail.com>
2026-01-30 23:45:10 +0530

    added console.log

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log('Count has changed:', count)
+  }, [count])
 
   return (
     <>
